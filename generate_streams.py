ac3fb4f282fd9ad2c20e3663c8b9f1a6d0ae0cca
Ehsan Elahi <eelahi@tssg.org>
2020-01-02 16:44:47 +0000

    This script is used to generate HTTP streams with the VLC server

--- generate_streams.py
+++ generate_streams.py
@@ -1,6 +1,15 @@
 '''
 This script reads the server csv file for start/stop time of each stream and creates the stream according to the start times
 The stream ends with the specified end time. 
+The CSV file is in this format:
+call_start_datetime,call_end_datetime,cpu_load_weight,memory_change_percentage,bytes_transmitted_mb,duration_seconds,hour,Day_of_the_Week,weekday_name
+2019-10-08 00:00:06,2019-10-08 00:44:05,1,3.2,33.0,2639.0,0,1,Tuesday
+2019-10-08 00:00:14,2019-10-08 00:01:33,1,0.2,4.0,79.0,0,1,Tuesday
+2019-10-08 00:00:18,2019-10-08 00:23:45,1,5.6,25.0,1407.0,0,1,Tuesday
+2019-10-08 00:00:19,2019-10-08 00:09:54,1,1.8,10.0,575.0,0,1,Tuesday
+2019-10-08 00:00:28,2019-10-08 00:02:47,1,0.4,6.0,139.0,0,1,Tuesday
+........
+........
 '''
 import time
 import datetime
